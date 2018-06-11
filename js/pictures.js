eb0d065bf6f9fb667f926f61cd5c2a998047efd9
SelivanovAN <superexpulsion@mail.ru>
2018-06-11 20:08:55 +0300

    Сделал задание на клонирование, надо еще все в функции обернуть

---
+++ js/pictures.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var COUNT_PHOTOS = 25;
+var MIN_LIKES = 15;
+var MAX_LIKES = 200;
+var PHOTO_COMMENTS = [
+  'Всё отлично',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+var PHOTO_DESCRIPTIONS = [
+  'Тестим новую камеру!',
+  'Затусили с друзьями на море',
+  'Как же круто тут кормят',
+  'Отдыхаем...',
+  'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
+  'Вот это тачка!'
+];
+
+// --------- функция генерации случайных чисел---------
+var getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+// --------- фукнция перемешивания массива ---------
+function shuffle(array) {
+  var j;
+  var temp;
+  var i;
+  for (i = array.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+// --------- Генерируется массив фоток количеством 25 шт из цикла (функция генерации случайных данных) ---------
+var photos = [];
+for (var j = 0; j < COUNT_PHOTOS; j++) {
+  photos.push({
+    url: 'photos/' + (j + 1) + '.jpg',
+    likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
+    comments: shuffle(PHOTO_COMMENTS).slice(0, getRandomNumber(0, PHOTO_COMMENTS.length - 1)),
+    description: PHOTO_DESCRIPTIONS[getRandomNumber(0, PHOTO_DESCRIPTIONS.length - 1)]
+  });
+}
+
+var gallery = document.querySelector('.pictures');
+var pictureTemplate = document.querySelector('#picture').content;
+
+// --------- Заполняем данными сгенерированные карточки (функция создания DOM-элемента на основе JS-объекта )---------
+var renderPhoto = function (photo) {
+  // функция создания DOM-элемента на основе JS-объекта
+  // функция заполнения блока DOM-элементами на основе массива JS-объектов
+  var photoElement = pictureTemplate.cloneNode(true);
+  photoElement.querySelector('.picture__img').src = photo.url;
+  photoElement.querySelector('.picture__stat--likes').textContent = photo.likes;
+  photoElement.querySelector('.picture__stat--comments').textContent = photo.comments.length;
+  return photoElement;
+};
+
+var fragment = document.createDocumentFragment();
+// --------- функция заполнения блока DOM-элементами на основе массива ---------
+for (var i = 0; i < photos.length; i++) {
+  fragment.appendChild(renderPhoto(photos[i]));
+}
+
+gallery.appendChild(fragment);
+
+var bigPicture = document.querySelector('.big-picture');
+
+var renderBigPicture = function (photo) {
+  bigPicture.querySelector('.big-picture__img img').src = photo.url;
+  bigPicture.querySelector('.likes-count').textContent = photo.likes;
+  bigPicture.querySelector('.comments-count').textContent = photo.comments.length;
+  bigPicture.querySelector('.social__caption').textContent = photo.description;
+  var socialComments = bigPicture.querySelector('.social__comments');
+  var socialComment = socialComments.querySelector('.social__comment').cloneNode(true);
+  socialComments.innerHTML = '';
+
+  var fragmentBigPicture = document.createDocumentFragment();
+
+  for (var l = 0; l < photo.comments.length; l++) {
+    var comment = socialComment.cloneNode(true);
+    comment.querySelector('.social__picture').src = 'img/avatar-' + getRandomNumber(1, 6) + '.svg';
+    comment.childNodes[comment.childNodes.length - 1].nodeValue = photo.comments[l];
+    fragmentBigPicture.appendChild(comment);
+  }
+  socialComments.appendChild(fragmentBigPicture);
+};
+renderBigPicture(photos[0]);
+bigPicture.classList.remove('hidden');
+
+var socialCommentCount = document.querySelector('.social__comment-count');
+var socialCommentLoad = document.querySelector('.social__comment-loadmore');
+socialCommentCount.classList.add('visually-hidden');
+socialCommentLoad.classList.add('visually-hidden');
